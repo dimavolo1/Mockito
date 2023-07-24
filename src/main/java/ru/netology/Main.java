bfbe01c25a08f1180407ed3e770422a48c068fd2
dimavolo1 <125883080+dimavolo1@users.noreply.github.com>
2023-07-24 11:07:11 +0500

    commit

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,24 @@
+package ru.netology;
+
+import java.util.Map;
+import java.util.HashMap;
+import ru.netology.geo.GeoService;
+import ru.netology.geo.GeoServiceImpl;
+import ru.netology.sender.MessageSender;
+import ru.netology.i18n.LocalizationService;
+import ru.netology.sender.MessageSenderImpl;
+import ru.netology.i18n.LocalizationServiceImpl;
+
+public class Main {
+
+    // Тестовый пример
+    public static void main(String[] args) {
+        GeoService geoService = new GeoServiceImpl();
+        LocalizationService localizationService = new LocalizationServiceImpl();
+        MessageSender messageSender = new MessageSenderImpl(geoService, localizationService);
+
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.123.12.19");
+        messageSender.send(headers);
+    }
+}
---
+++ src/main/java/ru/netology/sender/MessageSender.java
@@ -0,0 +1,9 @@
+package ru.netology.sender;
+
+
+import java.util.Map;
+
+public interface MessageSender {
+
+    String send(Map<String, String> headers);
+}
